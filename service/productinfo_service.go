41751340e1a8e0828158eb6e13670f3183ae072d
Jack Wang <weiwongfaye@gmail.com>
2021-12-16 22:06:53 +0800

    grpc server

---
+++ service/productinfo_service.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+
+	pb "service/ecommerce"
+
+	"github.com/gofrs/uuid"
+	"github.com/gogo/status"
+	"google.golang.org/grpc/codes"
+)
+
+// server is used to implement ecommerce/product_info.
+type server struct {
+	pb.UnimplementedProductInfoServer
+	productMap map[string]*pb.Product
+}
+
+// AddProduct implements ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out := uuid.NewV4()
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+
+}
+
+// GetProduct implements ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+
+}
